e132651ca6075860e9be44fea4e2b55898ae0f5e
D3XT237 <sergiolopezalon237@gmail.com>
2024-01-31 19:42:27 +0100

    31/1

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let modoJuegoPartida = 0;
 let fichasJugador1 = 0;
 let fichasJugador2 = 0;
 
+/*
+Crea la tabla con los botones e inicia el juego con la configuración indicada
+*/
 function iniciarJuego() {
     if (numFichasPartida == 0) {
         alert("Seleccione número de fichas");
@@ -17,7 +20,7 @@ function iniciarJuego() {
         for (let fila = 0; fila < 3; fila++) {
             tabla += "<tr>";
             for (let columna = 0; columna < 3; columna++) {
-                tabla += "<td><button class='botonTablero' id='fila" + fila + "columna" + columna + "' onclick='realizarMovimiento(" + fila + "," + columna + ")'></button></td>";
+                tabla += "<td><button class='botonTablero' id='fila" + fila + "columna" + columna + "' onclick='realizarMovimiento(" + fila + "," + columna + ", event)'></button></td>";
             }
             tabla += "</tr>";
         }
@@ -27,9 +30,47 @@ function iniciarJuego() {
     }
 }
 
-console.log();
+/*
+Comprueba el número de fichas y llama a la función en consecuencia
+*/
 
 function realizarMovimiento(fila, columna) {
+    if (numFichasPartida == 2) {
+        realizarMovimiento6fichas(fila, columna);
+    } else {
+        realizarMovimiento9fichas(fila, columna);
+    }
+}
+
+
+function realizarMovimiento6fichas(fila, columna) {
+    if (arrayJuego[fila][columna] == 0) {
+        if (fichasJugadorActual() < 3) {
+            sumarFichaJugador();
+            movimientosRealizados++;
+            arrayJuego[fila][columna] = jugadorActual;
+            cambiarBoton(fila, columna, jugadorActual);
+
+            if (verificarGanador()) {
+                alert("¡Jugador " + jugadorActual + " ha ganado!");
+                reiniciarJuego();
+            } else if (modoJuegoPartida == 1 && movimientosRealizados == 9) {
+                alert("¡Empate!");
+                reiniciarJuego();
+            } else {
+                cambiarTurno();
+            }
+        } else {
+            alert("Tienes 3 fichas. Debes quitar una antes de colocar una nueva.");
+        }
+    } else if (arrayJuego[fila][columna] == jugadorActual && fichasJugadorActual() == 3) {
+        restarFichaJugador();
+        arrayJuego[fila][columna] = 0;
+        cambiarBoton(fila, columna, 0);
+    }
+}
+
+function realizarMovimiento9fichas(fila, columna) {
     if (arrayJuego[fila][columna] == 0) {
         movimientosRealizados++;
         arrayJuego[fila][columna] = jugadorActual;
@@ -54,16 +95,28 @@ function realizarMovimiento(fila, columna) {
     }
 }
 
+function quitarFicha(fila, columna) {
+
+}
+
+/*
+Recibe los párametros de la posición a cambiar y el jugador que toma la posición y cambia lo que muestra el botón
+*/
 function cambiarBoton(fila, columna, jugadorActual) {
     let casilla = document.getElementById('fila' + fila + 'columna' + columna);
 
     if (jugadorActual == 1) {
         casilla.innerHTML = 'X';
-    } else {
+    } else if (jugadorActual == 2) {
         casilla.innerHTML = 'O';
+    } else {
+        casilla.innerHTML = '';
     }
 }
 
+/*
+Comprueba si hay un ganador y devuelve true si lo hay o false si no
+*/
 function verificarGanador() {
     for (let i = 0; i < 3; i++) {
         if (
@@ -84,6 +137,9 @@ function verificarGanador() {
     return false;
 }
 
+/*
+Devuelve todos los parámetros a como están al principio e inicia de nuevo la partida
+*/
 function reiniciarJuego() {
     arrayJuego = Array.from({ length: 3 }, () => Array(3).fill(0));
     jugadorActual = 1;
@@ -91,16 +147,26 @@ function reiniciarJuego() {
     iniciarJuego();
 }
 
+/*
+Cambia el color del botón seleccionado y asigna el número de fichas que indica el botón
+*/
 function elegirNumFichas(numFichas, id) {
     cambiarColorBotonFichas(id);
     numFichasPartida = numFichas;
 }
 
+/*
+Cambia el color del botón seleccionado y asigna modo de juego que indica el botón
+*/
 function elegirModoJuego(modoJuego, id) {
     cambiarColorBotonModo(id);
     modoJuegoPartida = modoJuego;
 }
 
+/*
+Cambia el color de los botones de número de fichas y se asegura de que solo pueda haber uno 'pulsado' para que el jugador/es sepan qué
+número de fichas está seleccionado
+*/
 function cambiarColorBotonFichas(id) {
     let botonPulsado = document.getElementById(id);
     let otroBoton;
@@ -119,6 +185,9 @@ function cambiarColorBotonFichas(id) {
     }
 }
 
+/*
+Cambia el color de los botones de modo de juego y se asegura de que solo pueda haber uno 'pulsado' para que el jugador/es sepan qué modo está seleccionado
+*/
 function cambiarColorBotonModo(id) {
     let botonPulsado = document.getElementById(id);
     let otroBoton1;
@@ -142,5 +211,53 @@ function cambiarColorBotonModo(id) {
         } else if (otroBoton2.classList.contains('pulsado')) {
             otroBoton2.classList.replace('pulsado', 'nopulsado');
         }
+    }
+}
+
+/*
+Comprueba cual es el jugador actual y devuelve el número de fichas que tiene colocadas
+*/
+function fichasJugadorActual() {
+    if (jugadorActual == 1) {
+        return fichasJugador1;
+    } else {
+        return fichasJugador2;
+    }
+}
+
+/*
+Le resta una ficha al jugador actual
+*/
+function restarFichaJugador() {
+    if (jugadorActual == 1) {
+        fichasJugador1--;
+    } else {
+        fichasJugador2--;
+    }
+}
+
+/*
+Le suma una ficha al jugador actual
+*/
+function sumarFichaJugador() {
+    if (jugadorActual == 1) {
+        fichasJugador1++;
+    } else {
+        fichasJugador2++;
+    }
+}
+
+/*
+Función para cambiar de turno
+*/
+
+function cambiarTurno() {
+    switch (jugadorActual) {
+        case 1:
+            jugadorActual = 2;
+            break;
+        case 2:
+            jugadorActual = 1;
+            break;
     }
 }
